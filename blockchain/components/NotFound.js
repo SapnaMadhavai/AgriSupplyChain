feb45a1655a2383fe9024c1e09f5a7ede723b083
Sapna Madhavai <136884694+SapnaMadhavai@users.noreply.github.com>
2024-07-05 16:54:42 +0530

    Delete blockchain/components directory

--- blockchain/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotFound = () =>
-    <div>
-        <h1>404 - Not Found!</h1>
-        <p>We are sorry but the page you are looking for does not exist.</p>
-    </div>
-
-export default NotFound;
